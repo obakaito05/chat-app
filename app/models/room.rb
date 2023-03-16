2821f9d28a40807ac552e71998a949ebfa5f011c
obakaito05 <1511530029j@gmail.com>
2023-03-16 18:05:14 +0900

    空のメッセージが送信できないバリデーションの設定

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,6 +1,6 @@
 class Room < ApplicationRecord
-  belongs_to :room
-  belongs_to :user
+  has_many :room_users
+  has_many :users, through: :room_users
   has_many :messages
 
   validates :name, presence: true
